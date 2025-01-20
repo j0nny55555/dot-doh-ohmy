549fbb51c1d5973d01ae09a3f5d4fe71402a3d0f
Jonny5 <jonny5@nova-labs.net>
2025-01-20 11:58:46 -0600

    updated FQDN list from valuable source

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dot-doh-ohmy
 A currated list of DoH and DoT FQDNs, you can use the included Shell script to "scrape" the IPs and generate your own list
 
+Recently added https://github.com/curl/curl/wiki/DNS-over-HTTPS FQDN list - massive update, will eventually automate import of FQDNs
+
 Notes:
 Requires 'dig' or 'drill' to resolve FQDNs to IPv4 and IPv6 addresses
 Puts all resolved IPs (v4 and v6) into the same list
--- dohFindIPs.sh
+++ dohFindIPs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-filename='dohDomainList.txt'
+filename='dohDomainListv3.txt'
 listOfIPs=()
 ipV4Regex="^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 ipV6Regex="(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
-echo $ipV6Regex
+
 while read domainName; do
+    domainName=$(echo "$domainName" | sed -E 's/#[[:space:]]*.*//')
+    echo "[=] Checking $domainName"
     for ipAddress in `dig +short -t A $domainName`; do
         if [[ $ipAddress =~ $ipV4Regex ]]; then
             echo "[+] DOH IPv4 $ipAddress"
